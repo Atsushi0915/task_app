55a9f2cb61da17ada7ba13d4a83d7997ba0b7145
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-04-11 22:28:37 +0900

    Admin::User controller 作成

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,10 @@
+class Admin::UsersController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/new'
+    get 'users/show'
+    get 'users/index'
+  end
   root  'tasks#index'
   resources :tasks
   # get 'task_new',  to:  'tasks#new',  as: 'new_task_path'
