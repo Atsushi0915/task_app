8463defc6b25e59da761784ba22f0df27bb44d92
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-04-17 10:38:41 +0900

    CSVインポート機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -55,6 +55,11 @@ class TasksController < ApplicationController
     render :new unless @task.valid?  #unless: 条件がfalseの時処理   valid？：エラーがある場合は false
   end
 
+  def import
+    current_user.tasks.import(params[:file])
+    redirect_to tasks_url, notice: "タスクを追加しました"
+  end
+
   private
     def task_params
       params.require(:task).permit(:name, :description, :image)
